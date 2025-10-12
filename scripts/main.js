7db186f37fe1bd49d7b47f0a21644d341b2a7351
Shannon Jaya <183021335+shannonjaya@users.noreply.github.com>
2025-10-12 17:49:13 -0400

    Move inline scripts to separate js file

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+// Filter 
+const options = [
+  "Chicken",
+  "Beef",
+  "Pork",
+  "Fish",
+  "Turkey",
+  "Vegetarian",
+];
+
+window.addEventListener("DOMContentLoaded", function () {
+  const filterCard = document.querySelector(".filter-card .filter-options");
+  if (filterCard) {
+    options.forEach((option) => {
+      const label = document.createElement("label");
+      label.className = "filter-option";
+      label.innerHTML = `
+        <input type="checkbox" name="filter" value="${option}">
+        <span class="checkmark"></span>
+        ${option}
+      `;
+      filterCard.appendChild(label);
+    });
+  }
+
+  // Clear filters 
+  const clearBtn = document.querySelector(".clear-filters-button");
+  if (clearBtn) {
+    clearBtn.onclick = function () {
+      document
+        .querySelectorAll('.filter-option input[type="checkbox"]')
+        .forEach((cb) => (cb.checked = false));
+    };
+  }
+
+  // Recipe card mockup
+  const card = document.querySelector(".recipe-card");
+  const container = document.querySelector(".recipe-cards");
+  if (card && container) {
+    for (let i = 0; i < 10; i++) {
+      container.appendChild(card.cloneNode(true));
+    }
+  }
+});
