545ecfd02a55c7e94ca1720a8e1f415eec4d6e64
Shannon Jaya <183021335+shannonjaya@users.noreply.github.com>
2025-12-10 15:54:15 -0500

    Fix navbar styling

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,19 @@
 <header>
-  <!-- Search in navbar -->
-  <a class="menu-item" id="home" href="index.php">Crave</a>
-  <div class="menu-items-right">
+  <!-- Brand/Logo -->
+  <div class="brand-container">
+    <a class="logo" id="home" href="index.php">Crave</a>
+  </div>
+  
+  <!-- Hamburger Menu Toggle -->
+  <input type="checkbox" id="nav-toggle" class="nav-toggle">
+  <label for="nav-toggle" class="nav-toggle-label">
+    <span></span>
+    <span></span>
+    <span></span>
+  </label>
+  
+  <!-- Navigation Menu -->
+  <nav>
     <form action="all-recipes.php" method="get" class="search-bar header-search">
       <input
         type="text"
@@ -19,8 +31,12 @@
       <button type="submit" aria-label="Search">
         <img src="assets/search.svg" alt="Search" width="24" height="24">
       </button>
-      </form>
-      <a class="menu-item" id="all-recipes" href="all-recipes.php">All Recipes</a>
-      <a class="menu-item" id= "help" href="index.php#help-section">Help</a>
-  </div>
+    </form>
+    <ul class="nav">
+      <li class="nav-divider"><span class="divider"></span></li>
+      <li><a href="all-recipes.php" class="menu-item">All Recipes</a></li>
+      <li class="nav-divider"><span class="divider"></span></li>
+      <li><a href="index.php#help-section" class="menu-item">Help</a></li>
+    </ul>
+  </nav>
 </header>
